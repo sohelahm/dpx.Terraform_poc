666ec5293f5e36f5eeb33e94ed738a63888301ed
sohelahm <SManiyar@eatonvance.com>
2025-11-20 15:47:44 +0530

    Add example variable definition in variable.tf

---
+++ variable.tf
@@ -0,0 +1,6 @@
+# Example variable file (not strictly needed for this PoC)
+variable "example_name" {
+  description = "An example variable"
+  type        = string
+  default     = "terraform-poc"
+}
